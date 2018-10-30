68cd24a2d4e2cb58fb90f229ce42dd3ecf58aa7c
alex <alexe_webmail@yahoo.com>
2018-10-29 17:25:51 -0700

    More updates to CAREP file header blocks

--- ModelVariables/MVStage.vb
+++ ModelVariables/MVStage.vb
@@ -1,5 +1,5 @@
 '*********************************************************************************************
-' Caribou Energy Protein: A SyncroSim Module for the Caribou Energy Protein Model.
+' Caribou Energy Protein: A SyncroSim Package for the Caribou Energy Protein Model.
 '
 ' Copyright © 2007-2018 Apex Resource Management Solution Ltd. (ApexRMS). All rights reserved.
 '
