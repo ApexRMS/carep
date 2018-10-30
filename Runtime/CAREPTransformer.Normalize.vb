0924a55f68501337a2f88a81d1b5de9ffa4f3167
alex <alexe_webmail@yahoo.com>
2018-10-29 17:23:17 -0700

    Update header blocks for all CAREP source files

--- Runtime/CAREPTransformer.Normalize.vb
+++ Runtime/CAREPTransformer.Normalize.vb
@@ -1,7 +1,7 @@
 ﻿'*********************************************************************************************
 ' Caribou Energy Protein: A SyncroSim Module for the Caribou Energy Protein Model.
 '
-' Copyright © 2007-2017 Apex Resource Management Solution Ltd. (ApexRMS). All rights reserved.
+' Copyright © 2007-2018 Apex Resource Management Solution Ltd. (ApexRMS). All rights reserved.
 '
 '*********************************************************************************************
 
